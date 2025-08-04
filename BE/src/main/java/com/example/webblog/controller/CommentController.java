3e191fc3bccb0738783052e858c6aa4912c721cb
Hoang Anh <phamnhuhoang24102004@gmail.com>
2025-08-05 00:23:22 +0700

    update comment controller

--- BE/src/main/java/com/example/webblog/controller/CommentController.java
+++ BE/src/main/java/com/example/webblog/controller/CommentController.java
@@ -2,9 +2,12 @@ package com.example.webblog.controller;
 
 import com.example.webblog.dto.request.CommentCreateRequest;
 import com.example.webblog.dto.request.CommentUpdateRequest;
+import com.example.webblog.dto.response.ApiResponse;
 import com.example.webblog.dto.response.CommentResponse;
 import com.example.webblog.service.Comment.CommentService;
+import com.example.webblog.util.ResponseHelper;
 import lombok.AllArgsConstructor;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
@@ -14,17 +17,29 @@ public class CommentController {
     private final CommentService commentService;
 
     @PostMapping
-    public CommentResponse createComment(@RequestBody CommentCreateRequest request){
-        return commentService.createComment(request);
+    public ResponseEntity<ApiResponse<CommentResponse>> createComment(@RequestBody CommentCreateRequest request){
+        return ResponseHelper.success(commentService.createComment(request), "Tao binh luan thanh cong");
     }
 
     @PutMapping("/{idComment}")
-    public CommentResponse updateComment(@PathVariable String idComment, @RequestBody CommentUpdateRequest request){
-        return commentService.updateComment(request, idComment);
+    public ResponseEntity<ApiResponse<CommentResponse>>
+                        updateComment(@PathVariable String idComment,
+                                      @RequestBody CommentUpdateRequest request){
+        return ResponseHelper.success(commentService.updateComment(request, idComment), "Cap nhap noi dung binh luan thanh cong");
     }
 
     @DeleteMapping("{idComment}")
-    public void deleteComment(@PathVariable String idComment){
+    public ResponseEntity<ApiResponse<Object>> deleteComment(@PathVariable String idComment){
         commentService.deleteComment(idComment);
+        return ResponseHelper.success("Xoa binh luan thanh cong");
+    }
+
+    @GetMapping
+    public ResponseEntity<ApiResponse<CommentResponse>> getComment(
+            @RequestParam(required = false) String postId,
+            @RequestParam(defaultValue = "0") int page,
+            @RequestParam(defaultValue = "10") int pageSize
+    ) {
+        return ResponseHelper.ofPage(commentService.getComments(postId, page, pageSize));
     }
 }
